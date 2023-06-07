2e40b524abff35d893af88db77338854b4faf8c9
sohyundoh <lyny.doh@gmail.com>
2023-06-07 19:45:12 +0900

    [lab/#2] SETTING | 첫번째 실험 initial setting
    Initial Setting

---
+++ ExternalApiPrac/src/main/java/com/sohyun/ExternalApiPrac/ExternalApiPracApplication.java
@@ -0,0 +1,13 @@
+package com.sohyun.ExternalApiPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalApiPracApplication.class, args);
+	}
+
+}
---
+++ ExternalApiPrac/src/test/java/com/sohyun/ExternalApiPrac/ExternalApiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohyun.ExternalApiPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
