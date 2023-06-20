b8f86dd8b10186adf70f09b750258f6cec008cc1
sohyundoh <lyny.doh@gmail.com>
2023-06-20 22:39:03 +0900

    [lab/#4] SETTING | 세번째 실험 initial setting

---
+++ Security/src/test/java/com/sohyun/springsecurityseminar/SpringSecuritySeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohyun.springsecurityseminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
